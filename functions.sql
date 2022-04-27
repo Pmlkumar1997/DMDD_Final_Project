0b0c981be663cea2e0e3fe16cb80dcbe29ea62ef
Sandeep Barla <66588653+SandeepBarla@users.noreply.github.com>
2022-04-26 20:19:29 -0400

    Added functions

--- functions.sql
+++ functions.sql
@@ -55,3 +55,50 @@ DBMS_OUTPUT.PUT_LINE('No of bookings: ' || v_bookings);
 RETURN v_bookings;
 END;
 /
+
+--number of tickets of each type sold per match
+
+CREATE OR REPLACE FUNCTION fixture_ticket_type_sold(tick_type VARCHAR2)
+RETURN sys_refcursor AS
+l_cursor sys_refcursor;
+begin
+  open l_cursor for 
+    select sum(ticket_count) as num_tickets, match_id,team1_id,team2_id, ticket_type 
+    from fixture_bookings where UPPER(ticket_type) = UPPER(tick_type)
+    group by match_id,team1_id,team2_id,ticket_type;
+  return l_cursor;
+
+END;
+
+
+--function to get the schedule of a league
+
+CREATE OR REPLACE FUNCTION getLeagueSchedule(league_nm IN VARCHAR2)
+RETURN sys_refcursor AS
+l_cursor sys_refcursor;
+v_league_nm VARCHAR2(30) := replace(initcap(league_nm),' ');
+begin
+dbms_output.put_line(league_nm);
+  open l_cursor for 
+    select match_id,team1_id,team2_id,match_date,venue_name,city from FIXTURE_SCHEDULE
+    where league_id = (select league_id from league where (replace(initcap(league_name),' ')) = v_league_nm);
+  return l_cursor;
+
+END;
+
+
+--function to get the revenue generated in the whole league
+
+CREATE OR REPLACE FUNCTION league_revenue(league_nm IN VARCHAR2)
+RETURN NUMBER
+AS 
+    v_league_revenue NUMBER(10);
+BEGIN
+
+select sum(revenue) INTO v_league_revenue from (select fx.match_id as match_id, SUM(fx.ticket_count*tc.ticket_price) as revenue from ADMIN.fixture_bookings fx
+join ADMIN.ticket_class tc on fx.ticket_type = tc.ticket_type
+where league_id = (select league_id from league where league_name = league_nm )
+group by match_id);
+
+RETURN v_league_revenue;
+END;
